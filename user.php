bc5fe906e51f8b9908350102116ed4f7eed318f0
MIDUA1 <zloy.black.hawk@gmail.com>
2020-04-24 20:41:31 +0300

    LanguageRegisterForm

--- user.php
+++ user.php
@@ -1,26 +1,61 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', true);
+var_dump($_POST);
+var_dump($_GET);
+error_reporting(E_ALL); // Показать все ошибки
+ini_set('display_errors', true); // Покажи на экране
+
+$lang = $_GET['lang'] ?? 'EN'; //https://www.php.net/manual/ru/migration70.new-features.php
+//$lang (!empty($_GET['lang'])) ? $_GET['lang'] : 'RU';
+$transform = [
+    'name' => ['EN' => 'Name','RU' => 'Имя','UA' => "Ім'я"],
+    'surname' => ['EN' => 'Surname','RU' => 'Фамилия','UA' => 'Прізвище'],
+    'age' => ['EN' => 'Age','RU' => 'Влозраст','UA' => 'Вік'],
+    'email' => ['EN' => 'E-mail', 'RU' => 'Э-почта', 'UA' => 'Е-пошта'],
+    'gender' => ['EN' => 'Gender', 'RU' => 'Пол', 'UA' => 'Стать'],
+    'enterbtn' => ['EN' => 'Enter', 'RU' => 'Отправить', 'UA' => 'Відправити'],
+    //'errorname' => ['EN' =>'1', 'RU' => '2', 'UA' =>'3'],
+
+];
 $error = [];
 if (!empty($_POST)) {
     if (empty($_POST['name'])) {
-        $error['name'] = 'Имя не может пустым';
+        $error['name'] =  'Введите имя';//transform['errorname'][$lang]
     }
     if (empty($_POST['surname'])) {
-        $error['surname'] = 'Фамилия не может пустой';
+        $error['surname'] = 'Введите фамилию';
     }
-    if (empty($_POST['age']) || $_POST['age'] < 1) {
-        $error['age'] = 'Возраст задан некорректно';
+    if (empty($_POST['age'])) {
+        $error['age'] = 'введите возраст';
     }
-    if (empty($_POST['email']) || $_POST['age'] ) {
-        $error[] = 'Возраст задан некорректно';
+        else if ($_POST['age'] < 16) {
+            $error['age'] = 'Подрости до 16';
+        }
+        else if ($_POST['age'] > 99) {
+            $error['age'] = 'Регистрация только для землян';
+        }
+
+    if (empty($_POST['email']) || $_POST['email'] == 'example@gmail.com' ) {
+        $error['email'] = 'введите почту';
     }
+
+//    if (empty($_POST['gender'])) {
+//        $error['gender'] = 'Ваш пол';
+//    }
+
 }
-$lang (!empty($_GET['lang'])) ? $_GET['lang'] : 'ru';
-$lables = [
-    'ru' => ['name' => 'Имя', ]
-];
-switch ()
+
+// switch ($lang)
+// {
+//     case 'RU':
+//            $translation = $transform['EN'];
+//         break;
+//     case  'UA':
+//         $translation = $transform['RU'];
+//         break;
+//     case 'EN':
+//         $translation = $transform['UA'];
+//         break;
+// }
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -34,64 +69,85 @@ switch ()
 <br/>
 <h1>Форма регистрации</h1>
 <div class="container">
-<!--    <h3 style="color:red">--><?//=implode("<br />", $error) ?><!--</h3>-->
+
     <div class="float-right">
+        <a href="http://localhost:8080/user.php" class="badge badge-danger">Default</a>
+        <a href="?lang=EN" class="badge badge-success">Английский</a>
         <a href="?lang=RU" class="badge badge-primary">Русский</a>
         <a href="?lang=UA" class="badge badge-secondary">Украинский</a>
-        <a href="?lang=EN" class="badge badge-success">Английский</a>
     </div>
     <form method="post" action="user.php">
         <div class="form-group">
-            <label for="formGroupExampleInput">Имя</label>
-            <input type="text" class="form-control" id="formGroupExampleInput" name="name" placeholder="Example input"
-                   value="<?php echo $_POST['name'] ?? 'Mike' ?>">
-            <?php if(!empty($error['name'])) : ?>
-            <small id="passwordHelpBlock" class="form-text text-muted">
-                <?=$error['name'] ?>
-            </small>
-            <?php endif; ?>
+            <label for="formGroupExampleInput"><?=$transform['name'][$lang] ?>
+                <?php if(!empty($error['name'])) : ?>
+                    <small id="passwordHelpBlock" class="alert alert-danger">
+                        <?=$error['name'] ?>
+                    </small>
+                <?php endif; ?>
+            </label>
+            <input type="text" class="form-control" id="formGroupNameInput" name="name" placeholder=""
+                   value="<?php echo $_POST['name'] ?? '' ?>">
+
         </div>
         <div class="form-group">
-            <label for="formGroupExampleInput">Фамилия</label>
-            <input type="text" class="form-control" id="formGroupExampleInput" name="surname"
-                   placeholder="Example input" value="<?= $_POST['surname'] ?? 'Kardakov' ?>">
-            <?php if(!empty($error['surname'])) : ?>
-                <small id="passwordHelpBlock" class="form-text text-muted">
-                    <?=$error['surname'] ?>
-                </small>
-            <?php endif; ?>
+            <label for="formGroupExampleInput"><?=$transform['surname'][$lang] ?>
+                <?php if(!empty($error['surname'])) : ?>
+                    <small id="passwordHelpBlock" class="alert alert-danger">
+                        <?=$error['surname'] ?>
+                    </small>
+                <?php endif; ?>
+            </label>
+            <input type="text" class="form-control" id="formGroupSurnameInput" name="surname"
+                   placeholder="" value="<?= $_POST['surname'] ?? '' ?>">
+
         </div>
         <div class="form-group">
-            <label for="formGroupExampleInput">Возраст</label>
-            <input type="number" class="form-control" id="formGroupExampleInput" name="age" placeholder="Example input"
-                   value="<?= $_POST['age'] ?? '20' ?>">
-            <?php if(!empty($error['age'])) : ?>
-                <small id="passwordHelpBlock" class="form-text text-muted">
-                    <?=$error['age'] ?>
-                </small>
-            <?php endif; ?>
+            <label for="formGroupExampleInput"><?=$transform['age'][$lang] ?>
+                <?php if(!empty($error['age'])) : ?>
+                    <small id="passwordHelpBlock" class="alert alert-danger">
+                        <?=$error['age'] ?>
+                    </small>
+                <?php endif; ?>
+            </label>
+            <input type="number" class="form-control" id="formGroupAGEInput" name="age" placeholder=""
+                   value="<?= $_POST['age'] ?? '' ?>">
+
         </div>
         <div class="form-group">
-            <label for="formGroupExampleInput">Почта</label>
-            <input type="email" class="form-control" id="formGroupExampleInput" name="email" placeholder="Example input"
+            <label for="formGroupExampleInput"><?=$transform['email'][$lang] ?>
+                <?php if(!empty($error['email'])) : ?>
+                    <small id="passwordHelpBlock" class="alert alert-danger">
+                        <?=$error['email'] ?>
+                    </small>
+                <?php endif; ?>
+            </label>
+            <input type="email" class="form-control" id="formGroupMailInput" name="email" placeholder=""
                    value="<?= $_POST['email'] ?? 'example@gmail.com' ?>">
-            <?php if(!empty($error['email'])) : ?>
-                <small id="passwordHelpBlock" class="form-text text-muted">
-                    <?=$error['email'] ?>
-                </small>
-            <?php endif; ?>
+
         </div>
+<!--  Пока не работающий блок. Не могу разобраться, но ПОСТ определенно тут не делает то что надо-->
         <div class="form-group">
-            <label for="exampleFormControlSelect1">Example select</label>
-            <select multiple="multiple" class="form-control" id="exampleFormControlSelect1" name="gender[]">
+            <label for="exampleFormControlSelect1"><?=$transform['gender'][$lang] ?>
+                <?php if(!empty($error['gender'])) : ?>
+                    <small id="passwordHelpBlock" class="alert alert-danger">
+                        <?=$error['gender'] ?>
+                    </small>
+                <?php endif; ?>
+            </label>
+            <select  class="form-control" id="exampleFormControlSelect1" name="gender">
                 <option></option>
-                <option>Man</option>
-                <option>Woman</option>
-                <option>Others</option>
+                <option<?=$_POST['gender'] == 'Man' ? 'selected': '' ?>>Man</option>
+                <option<?=$_POST['gender'] == 'Woman' ? 'selected': '' ?>>Woman</option>
+                <option<?=$_POST['gender'] == 'OtHers' ? 'selected': '' ?>>OtHers</option>
+                <?php if(!empty($error['gender'])) : ?>
+                    <small id="passwordHelpBlock" class="form-text text-muted">
+                        <?=$error['gender'] ?>
+                    </small>
+                <?php endif; ?>
 
             </select>
         </div>
-        <button type="submit" class="btn btn-primary">Отправить</button>
+        <button type="submit" class="btn btn-primary"><?=$transform['enterbtn'][$lang] ?></button>
     </form>
 </div>
 </body>
